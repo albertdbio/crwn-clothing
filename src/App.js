8b9b12e70c6f339e9eaaa5e9a78b5bb0936d439e
Albert Martinez <ioalbertdb@gmail.com>
2021-12-07 13:10:47 -0800

    Setup MenuItem with react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
 import HomePage from "./pages/homepage/homepage.component";
-import './App.css'
+import { Route, Switch } from "react-router-dom";
+
+import "./App.css";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 function App() {
-  return <HomePage/>;
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />        
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
